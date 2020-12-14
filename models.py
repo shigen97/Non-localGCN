3f1752c3f9902e64f3a20a76c75543faf0c2bbbb
shigen97 <75985950+shigen97@users.noreply.github.com>
2020-12-14 22:36:17 +0800

    Update models.py

--- models.py
+++ models.py
@@ -106,22 +106,6 @@ class NonLocalGCNLayer(nn.Module):
         self.activation = activation
 
     def forward(self, bg, feats):
-        """Update atom representations
-
-        Parameters
-        ----------
-        bg : BatchedDGLGraph
-            Batched DGLGraphs for processing multiple molecules in parallel
-        feats : FloatTensor of shape (N, M1)
-            * N is the total number of atoms in the batched graph
-            * M1 is the input atom feature size, must match in_feats in initialization
-
-        Returns
-        -------
-        new_feats : FloatTensor of shape (N, M2)
-            * M2 is the output atom feature size. If self.agg_mode == 'flatten', this would
-              be out_feats * num_heads, else it would be just out_feats.
-        """
         new_feats = self.gnn(bg, feats)
         if self.agg_mode == 'flatten':
             new_feats = new_feats.flatten(1)
